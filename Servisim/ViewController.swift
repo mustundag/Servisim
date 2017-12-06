6d1a074522e93c21d1ec2a1a83682c0b36639655
Muhittin Üstündağ <mustundag@iski.gov.tr>
2017-12-06 11:20:56 +0300

    Initial Commit

---
+++ Servisim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Servisim
+//
+//  Created by muhittin üstündağ on 6.12.2017.
+//  Copyright © 2017 Muhittin Ustundag. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
